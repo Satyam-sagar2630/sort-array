fa985949fdb19c685f0a2a5e803208682eb7b79f
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-21 00:13:48 +0530

    Add files via upload

---
+++ sort-array/int array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[]arg)
+        {
+            int[] num = { 1, 2, 5, 3, 1, };
+            Array.Sort(num);
+            foreach(int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
